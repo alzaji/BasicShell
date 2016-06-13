d916b84656b8d71f35ec021ac48a5e299b597470
HAZJ <alzaji@gmail.com>
2016-06-13 05:28:09 +0200

    Versión final documentada
    Shell_project.c completamente documentado.

--- Shell_project.c
+++ Shell_project.c
@@ -12,6 +12,9 @@ To compile and run the program:
    $ ./Shell
 	(then type ^D to exit program)
 
+Autor: Alberto Zamora Jiménez
+			 2º Grado en Ingeniería Informática "A"
+
 **/
 
 #include "job_control.h"   // remember to compile with module job_control.c
@@ -36,37 +39,42 @@ To compile and run the program:
 // -----------------------------------------------------------------------
 
 job * backjobs; // global list of jobs
-struct termios termconf; /* will contain shell's config in order to restore later */
-struct termios termpidconf; /* process terminal config */
-int shell_terminal; /* terminal file descriptor */
+struct termios termconf; /* Estructura termios que guardara nuestra config original */
+struct termios termpidconf; /* Estructura termios que guardara la config del proceso */
+int shell_terminal; /* Variable para almacenar el descriptor de fichero de la terminal */
+
+//------------------------------------------------------------------------
+//                     Manejador de SIGCHILD
+//------------------------------------------------------------------------
+
 
 void sigchldhandler(){
-	int i;
 	int info;
 	int status;
 	enum status status_analyzed;
-	pid_t pid_aux, wpid;
-	job * item = backjobs->next; // points to the next item in the list
+	pid_t pid_aux, wpid; // Variables para gestionar pids
+	job * item = backjobs->next; // Apunta al siguiente item en la lista
 
-  printf("\nSIGHCLD arrived...\n");
+  printf("\nSIGCHILD invocado...\n"); // Informamos de la llegada de la señal
   block_SIGCHLD();
 
-	if(empty_list(backjobs)!=1){
+	if(empty_list(backjobs)!=1){//Mientras la lista no este vacia la recorremos
 		while(item != NULL){
-			pid_aux = item->pgid; //points to pgid
-			wpid = waitpid(pid_aux, &status, WNOHANG | WUNTRACED); // let's check if the process is alive or if it has stopped
-			if(wpid==pid_aux){
+			pid_aux = item->pgid; //Dame el pgid del item
+			wpid = waitpid(pid_aux, &status, WNOHANG | WUNTRACED); // Informame de como esta el proceso
+			if(wpid==pid_aux){ //Si termino o fue parado, analizamos el estado
 				status_analyzed=analyze_status(status, &info);
-				printf("\nJob pid: %d, Command: %s, %s, Info: %d\n",pid_aux,item->command,status_strings[status_analyzed],info);
-				if(status_analyzed==SUSPENDED){ // handle SUSPENDED here
-					item->state=STOPPED; //Stop the process
-					item=item->next; // point to the next one
-				}else{ //handle SIGNALED and EXITED here
-					job * aux = item;  // define a new job so we don't loose the reference
-					item=item->next; // item points to the next one in list
-					delete_job(backjobs, aux); // get the job out the list
+				printf("\nJob pid: %d, Command: %s, %s, Info: %d\n",pid_aux,item->command,status_strings[status_analyzed],info);// Informa de su estado
+				if(status_analyzed==SUSPENDED){ // Aquí manejamos SUSPENDED
+					item->state=STOPPED; //Cambia el estado del proceso a STOPPED
+					item=item->next; // Apunta item al siguiente elemento
+				}else{ // Aquí manejamos SIGNALED y EXITED
+					job * aux = item;  // Define un job nuevo para no perder la referencia
+					item=item->next; // Avanzamos item al siguiente
+					delete_job(backjobs, aux); // Eliminamos este item de la lista
+
 				}
-			}else{
+			}else{// Si no termino o no fue parado, pasamos al siguiente
 				item=item->next;
 			}
 		}
@@ -84,142 +92,199 @@ int main(void)
 	char *args[MAX_LINE/2];     /* command line (of 256) has max of 128 arguments */
 
 	// probably useful variables:
-	int pid_fork, pid_wait; /* pid for created and waited process */
+	int pid_fork; /* pid for created */
 	int status;             /* status returned by wait */
 	enum status status_res; /* status processed by analyze_status() */
 	int info;				/* info processed by analyze_status() */
-	char time_date[128];	/* time and date for prompt*/
-	char path[100];			/* path for prompt*/
+	char time_date[128];	/* Fecha y hora para nuestro prompt*/
+	char path[100];			/* ruta para nuestro prompt */
 
-	backjobs = new_list("Background_jobs");
-	signal(SIGCHLD,sigchldhandler);
-	ignore_terminal_signals();
-	shell_terminal = STDIN_FILENO;
+	backjobs = new_list("Background_jobs"); // Creamos nuestra lista de jobs;
+	signal(SIGCHLD,sigchldhandler); // Instalamos el manegador de SIGCHILD
+	ignore_terminal_signals(); // Prevenimos el lanzamiento de ^C ^Z...
+	shell_terminal = STDIN_FILENO; //Descriptor de fichero de entrada de la terminal usado para tcgetattr
 
-  tcgetattr(shell_terminal,&termconf); /* Store the current shell's config */
-	termpidconf = termconf;
+	tcgetattr(shell_terminal,&termconf); /* Guardamos la configuración actual de la terminal */
+	termpidconf = termconf; // Inicializamos termpidconf con la misma config, sera modificada posteriormente
 
-	//Algo de info por aquí
+	/*Mostramos algo de información sobre la versión, autoría e indicamos el comando de ayuda */
 	printf(ANSI_COLOR_RED "Shell básica V1.0\n");
 	printf(ANSI_COLOR_GREEN "Alberto Zamora Jiménez - 2º Grado en Ingeniería Informatica A\n");
 	printf(ANSI_COLOR_GREEN "Escriba help para una lista de comandos internos de esta shell.\n" );
 
 	while (1)   /* Program terminates normally inside get_command() after ^D is typed */
 	{
-    /* The next block sets some config for the prompt, as we don't have any external config
-    file (i.e .bashrc) around nor functions to set it in place.*/
+		/* El siguiente bloque configura el prompt al gusto del autor. Puesto que no disponemos
+		 * de un fichero tipo .bashrc, lo hacemos a mano.
+		 * El resultado sería algo como [13/06/16 03:27:36] :/home/alberto/D_DRIVE/Universidad/2015/Sistemas Operativos/Shell/Ampliada$
+		 */
 
-		realpath("./", path); //resolves our current path pointer and returns a string in path
+		realpath("./", path); //Resolvemos nuestro puntero de ruta actual y devolvemos el resultado en forma de cadena a path
 		time_t tiempo = time(0);
-		struct tm *tlocal = localtime(&tiempo); //converts tiempo to a tm struct so we can manipulate it later
-		strftime(time_date,128,"[%d/%m/%y %H:%M:%S]", tlocal); // format tlocal as we desire and retun a string to time_date
-		printf(ANSI_COLOR_CYAN "%s :%s$ %s", time_date, path, ANSI_COLOR_BLACK); // set our prompt to cyan with the current date, time and path
+		struct tm *tlocal = localtime(&tiempo); //Convierte tiempo a una estructura tm para manipularla posteriormente
+		strftime(time_date,128,"[%d/%m/%y %H:%M:%S]", tlocal); // Le damos el formato deseado y lo guardamos en time_date
+		printf(ANSI_COLOR_CYAN "%s :%s$ %s", time_date, path, ANSI_COLOR_BLACK); // Ponemos nuestro prompt con color cian y mostramos fecha, hora y ruta actual
 		fflush(stdout);
 		get_command(inputBuffer, MAX_LINE, args, &background);  /* get next command */
 
-		if(args[0]==NULL) continue;   // if empty command
+		if(args[0]==NULL) continue;    // Si el commando es vacio, continuamos
 
-		if(strcmp(args[0], "help")==0){ // prints a detailed list of built-in commands
+		//------------------------------------------------------------------------
+		//                     		Comandos internos
+		//------------------------------------------------------------------------
+
+		/* Comando interno "help"
+		 *-----------------------
+		 * Cuando el usuario llame a help, mostraremos por pantalla información detallada sobre
+		 * comandos disponibles y comportamiento de ciertos aspectos de la shell.
+		 */
+
+		if(strcmp(args[0], "help")==0){
 			printf(ANSI_COLOR_RED "Comandos disponibles: \n");
 			printf(ANSI_COLOR_GREEN "fg ->" ANSI_COLOR_BLACK " Pone un job en primer plano. Admite como argumento el número de job que desea poner en primer plano. Si se ejecuta sin argumentos cogerá el último job introducido en la lista de jobs.\n");
 			printf(ANSI_COLOR_GREEN "bg ->" ANSI_COLOR_BLACK " Pone un job en segundo plano. Admite como argumento el numero de job que desea poner en segundo plano. Si se ejecuta sin argumentos cogerá el último job introducido en la lista de jobs.\n");
 			printf(ANSI_COLOR_GREEN "jobs ->" ANSI_COLOR_BLACK " Imprime una lista detallada de los jobs pertenecientes a esta shell.\n");
 			continue;
 		}
+
+		/* Comando interno "jobs"
+	 * ---------------------
+	 * Cuando el usuario llame a jobs, mostraremos por pantalla la lista de trabajos
+	 */
+
 		if(strcmp(args[0], "jobs")==0){ // jobs implementation here, only prints our background jobs list.
 			print_job_list(backjobs);
 			continue;
 		}
-		else if(strcmp(args[0], "fg")==0){ // fg built-in command, gets a process from background to foreground, behaviour is similar as bash's fg
+
+		/*Comando interno "fg"
+	 *-------------------
+	 * Trae un proceso a primer plano.
+	 * El modo de empleo es similar a bash, si se lanza sin argumentos cogerá el primer elemento de la lista,
+	 * Como argumento acepta el numero de job que se desea poner en primer plano.
+	 */
+
+		else if(strcmp(args[0], "fg")==0){
 			int position;
 			enum status status_a;
 			char * comando;
-			int listasize = backjobs->pgid;
 
-			if(args[1]==NULL){ // if no arguments, get the first job from the list
+			if(args[1]==NULL){ // Si no hay argumentos, coge el primero de la lista
 				position=1;
-			}else position = atoi(args[1]); // atoi() gets the argument and transforms it into an integer
-				if(position<0 || position>listasize){ // if position is invalid, keep going.
+			}else position = atoi(args[1]); // Sino usamos atoi() para coger el argumento y transformarlo en un entero manipulable
+				if(position<0 || position>list_size(backjobs)){ // Si la posicion es erronea no hacemos nada y continuamos en el bucle
 					continue;
 				}
-				job * fgjob = get_item_bypos(backjobs, position); // get the item from the list
-				comando = fgjob->command; // get the command issued in this job
-				int fgpid = fgjob->pgid; // get pgid for the job
-				set_terminal(fgpid); // give the terminal to the job
-				tcsetattr(shell_terminal, TCSANOW, &termpidconf);
-				if(fgjob->state==STOPPED){  // handle suspended jobs
-					killpg(fgpid, SIGCONT); // send sigcont to the group so it awakes.
+				job * fgjob = get_item_bypos(backjobs, position); // Coge el elemento de la lista
+				comando = fgjob->command; // Coge el comando lanzado en este proceso
+				int fgpid = fgjob->pgid; // Cogemos el pgid
+				set_terminal(fgpid); // Le damos el terminal al job
+				tcsetattr(shell_terminal, TCSANOW, &termpidconf); // Restauramos la configuracion del proceso
+				if(fgjob->state==STOPPED){  // Manejo de trabajos suspendidos
+					killpg(fgpid, SIGCONT); // Mandamos SIGCONT para que el proceso continue con su ejecucion
 				}
-				fgjob->state = FOREGROUND; // set job state to FOREGROUND
-				waitpid(fgpid, &status, WUNTRACED); // return if child has stopped and tell me what happened
-				status_a=analyze_status(status, &info); // get some info
-				printf("Foreground pid: %d, Command: %s, %s, Info: %d\n",fgpid,comando,status_strings[status_a],info); // print it
-				set_terminal(getpid()); //give the terminal to the shell
-				tcsetattr(shell_terminal, TCSANOW, &termconf);
-				if(status_a==SUSPENDED){ // if the job was suspended, set state to STOPPED
+				fgjob->state = FOREGROUND; // Ponemos el estado del proceso a FOREGROUND
+				waitpid(fgpid, &status, WUNTRACED); // Vuelve si el hijo ha parado y dime qué ha pasado
+				status_a=analyze_status(status, &info); // analizamos la información
+				printf("Foreground pid: %d, Command: %s, %s, Info: %d\n",fgpid,comando,status_strings[status_a],info);// Informamos al usuario
+				set_terminal(getpid());  //Devolvemos la terminal al shell
+				tcsetattr(shell_terminal, TCSANOW, &termconf);//Restauramos la configuracion del shell
+				if(status_a==SUSPENDED){ // Si se suspendio el proceso cambiamos el estado a STOPPED
 					fgjob->state=STOPPED;
-				}else delete_job(backjobs, fgjob); //else it was signaled or it exited.
+				}else delete_job(backjobs, fgjob); //Si no fue así, lo sacamos de la lista
 				continue;
 		}
-		else if(strcmp(args[0], "bg")==0){ //bg built-in command, gets a suspended process from the jobs list and puts it to background, similar behaviour as bash's bg
+		/*Comando interno "bg"
+		 *-------------------
+		 * Indica un proceso en segundo plano que continue su ejecución.
+		 * Si el proceso que se encuentra en nuesta lista fue suspendido, le indicamos que continue su ejecución.
+		 * El modo de empleo es similar a bash, si se lanza sin argumentos cogerá el primer elemento de la lista,
+		 * Como argumento acepta el numero de job que se desea poner en primer plano.
+		 */
+		else if(strcmp(args[0], "bg")==0){
 			int position;
-			if(args[1]==NULL){ //if no arguments, get the first gob from the list
+			if(args[1]==NULL){ //Si no hay argumentos, coge el primero de la lista
 				position=1;
-			}else position = atoi(args[1]); // atoi() gets the argument and transforms it to an integer
-				if(position<0 || position>list_size(backjobs)){ //if position is invalid, keep going.
+			}else position = atoi(args[1]); // Sino usamos atoi() para coger el argumento y transformarlo en un entero manipulable
+				if(position<0 || position>list_size(backjobs)){ //Si la posicion es erronea no hacemos nada y continuamos en el bucle
 					continue;
 				}
-				job * fgjob = get_item_bypos(backjobs, position); // get the item from the list
-				if(fgjob->state==STOPPED){ //if it is suspended, send a signal to awake it.
-					int fgpid = fgjob->pgid; // get the pgid of the job
-					fgjob->state=BACKGROUND; // set state to BACKGROUND
-					killpg(fgpid, SIGCONT); // tell it to continue
-					printf("Background job runing.. pid: %d, Command: %s\n",fgpid,fgjob->command); // print info about what happened
+				job * fgjob = get_item_bypos(backjobs, position); // Coge el elemento de la lista
+				if(fgjob->state==STOPPED){ // Si esta suspendido cambiamos el estado a BACKGROUND Y le mandamos SIGCONT para que continue
+					int fgpid = fgjob->pgid;  // Cogemos el pgid
+					fgjob->state=BACKGROUND;
+					killpg(fgpid, SIGCONT);
+					printf("Background job runing.. pid: %d, Command: %s\n",fgpid,fgjob->command); // Informamos de lo que ha sucedido
 
 			}
 				continue;
 		}
+
+		/*Comando interno "cd"
+		 *--------------------
+		 *cd es un comando interno típico, se usa para cambiar al directorio indicado pasado por argumento
+		 */
+
 		else if(strcmp(args[0], "cd")==0){ //cd built-in command, changes directory to the one passed as argument
 					chdir(args[1]);
 					continue;
 				}
+
+	//------------------------------------------------------------------------
+	//                     Lanzamiento de comandos externos
+	//------------------------------------------------------------------------
+	 /* La trazabilidad se resume en lo siguiente:
+	 *
+	 * - Hacemos el fork y comprobamos si se creó.
+	 * - Si no lo hizo, lanzamos un error por pantalla.
+	 * - Si el proceso hijo se creo, creamos el nuevo grupo de procesos con el pid obtenido por getpid() y restauramos señales ^C ^Z...
+	 * - Si el proceso fue lanzado en Foreground (background == 0), le damos la terminal
+	 * - Lanzamos execvp con el nombre de proceso y sus argumentos
+	 * - Si se lanzo en foreground, le damos la terminal y realizamos un waitpid bloqueante hasta que salga, posteriormente le devolvemos la terminal
+	 *   al shell, restauramos su configuracion e informamos del estado del proceso. Si fue suspendido, Lo añadimos a la lista de jobs indicando
+	 *   dicho estado.
+	 * - Si se lanzo en background (background == 1), simplemente le indicamos al usuario que fue lanzado y lo añadimos a la lista de jobs poniendo su estado a BACKGROUND
+	 */
+
 		else{
-			pid_fork=fork(); //creates a new process
-			if(pid_fork<0){	//ERROR
+			pid_fork=fork(); //Creamos el proceso nuevo
+			if(pid_fork<0){	// Si falla el fork, informamos por aquí y mandamos un exit(1) hacia arriba
 				printf("ERROR: Forking child error\n");
 				exit(1);
 
 			}else if(pid_fork==0){	//Child process
-				new_process_group(getpid());	//Create new process group for pid child
-				restore_terminal_signals(); // let me use ^C, ^Z...
+				new_process_group(getpid());	// Si tuvo exito lanzamos el proceso
+				restore_terminal_signals();  // Restauramos señales para poder usar ^C ^Z...
 				if(background==0){ //Foreground
           set_terminal(getpid());
 				}
 
-				if(execvp(*args, args)<0){
+				if(execvp(*args, args)<0){ //si no se ejecuto execvp lanzamos un error
 					printf("ERROR: Child process error\n");
 					exit(1);
 				}
 
 				}else{
-					if(background==0){ // no '&' was found, the process has the terminal
-            set_terminal(pid_fork); // give the new process the terminal
-						tcgetattr(shell_terminal, &termpidconf);
-						waitpid(pid_fork, &status, WUNTRACED); // return if child has stopped and tell me what happened
+					if(background==0){ // No se encontro '&' el proceso tiene el terminal
+            set_terminal(pid_fork); // Dale al proceso el terminal
+						tcgetattr(shell_terminal, &termpidconf); // Toma su configuración de terminal
+						waitpid(pid_fork, &status, WUNTRACED); // Vuelve si el hijo ha parado y dime que ha ocurrido
 						status_res=analyze_status(status, &info);
-						printf("Foreground pid: %d, Command: %s, %s, Info: %d\n",pid_fork,*args,status_strings[status_res],info); // tell the user some info about the job
-						set_terminal(getpid()); // get the terminal back
-						tcsetattr(shell_terminal, TCSANOW, &termconf); // restores original terminal configuration
-						if(status_res==SUSPENDED){
-							job * this_job = new_job(pid_fork, args[0], STOPPED); // set this job as STOPPED
+						printf("Foreground pid: %d, Command: %s, %s, Info: %d\n",pid_fork,*args,status_strings[status_res],info); // Informa al usuario
+						set_terminal(getpid()); // Devuelve la terminal al shell
+						tcsetattr(shell_terminal, TCSANOW, &termconf); // Restaura la configuración del terminal original
+						if(status_res==SUSPENDED){ // si fue suspendido añadelo indicando que su estado es STOPPED
+							job * this_job = new_job(pid_fork, args[0], STOPPED); // estado == STOPPED
               block_SIGCHLD();
-							add_job(backjobs, this_job); // add this job to the list
+							add_job(backjobs, this_job); // Lo añadimos a la lista
               unblock_SIGCHLD();
 						}
-					}else{ // backgroud == 1,
-						printf("Background job running.. pid: %d, Command: %s\n",pid_fork,*args); // tell the user some info about the job
-						job * this_job = new_job(pid_fork, args[0], BACKGROUND); // Set this job as BACKGROUND
-						add_job(backjobs, this_job); // add this job to the list
+					}else{ // Se encontro '&', background == 1, el proceso esta en segundo plano
+						printf("Background job running.. pid: %d, Command: %s\n",pid_fork,*args);  // Informa al usuario
+						job * this_job = new_job(pid_fork, args[0], BACKGROUND); // Estado == BACKGROUND
+						block_SIGCHLD();
+						add_job(backjobs, this_job); // lo añadimos a la lista
+						unblock_SIGCHLD();
 					}
 
 			}
